b4035869835a70a136bddff80e22174d45e58ac7
Softdev <softdev787@gmail.com>
2024-12-03 23:51:46 -0800

    change backend end point

--- src/utils/getBackendURL.js
+++ src/utils/getBackendURL.js
@@ -1,6 +1,4 @@
 export const getBackendURL = () => {
-  const apiUrl = process.env.REACT_APP_BACKEND_URL || "apiurl";
-  return process.env.NODE_ENV === "production"
-    ? apiUrl
-    : "http://localhost:8080";
+  const apiUrl = process.env.REACT_APP_BACKEND_URL;
+  return apiUrl;
 };
